da512624be4171f49e6128638a0d52ae11623761
PenWallet <carterasindinero@gmail.com>
2017-10-24 13:08:45 +0200

    Inicio de división porretas

---
+++ Boletin 3B/boletin3b_9_divisionporrestas.java
@@ -0,0 +1,30 @@
+/* Análisis:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Requisitos
+ * 
+ * 
+ * 
+ * 
+*/
+
+/* Pseudocódigo Generalizado:
+ * 
+ * 
+ * 
+ * 
+ * 
+*/
+
+import java.util.Scanner;
+import java.io.*;
+
+public class boletin3b_1_numerosnaturales 
+{
+	
+	public static void main (String[] args) 
+	{
+		
