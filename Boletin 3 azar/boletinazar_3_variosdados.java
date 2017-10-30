fb0f2b56c105f098afbe3974cb1073525cfb4921
PenWallet <carterasindinero@gmail.com>
2017-10-30 14:41:02 +0100

    Noje

---
+++ Boletin 3 azar/boletinazar_3_variosdados.java
@@ -0,0 +1,37 @@
+/* Análisis:
+ * Se tirarán dos dados a la vez, y se imprimirá en la pantalla
+ * la cantidad de veces que salen dos caras, dos cruces, o uno de cada
+ * 
+ * Entrada:
+ * - Si desea ejecutar el programa (Y / N)
+ * - 
+ * 
+ * 
+ * Salida
+ * - Cantidad de veces que ha salido un número
+ * - Porcentajes de cada número
+ * 
+ * 
+ * Pseudocódigo generalizado:
+ * 	Inicio
+ * 
+ * 	Fin
+ * 	
+ * 
+ * 	
+ */
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.Random;
+
+public class boletinazar_3_variosdados 
+{	
+	public static void main (String[] args) 
+	{
+		//Declarar variables de inicio
+		char bucleinicio;
+		Scanner teclado = new Scanner (System.in);
+		Random tirardado = new Random();
+		
+		
