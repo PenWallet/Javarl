6a2a7ab4400775fc8e6a9aebef73f05400d03575
PenWallet <carterasindinero@gmail.com>
2017-11-03 09:18:15 +0100

    Empezado FOL_Salario

---
+++ fol_salario.java
@@ -0,0 +1,78 @@
+/* Nombre del programa: fol_salario
+ * 
+ * Análisis:
+ * El usuario podrá hacer las sumas de los devengos, las deducciones, el salario bruto,
+ * el salario líquido, el BCCC y el BCCP .
+ * 
+ * Se parará de leer datos cuando el usuario introduzca un número negativo.
+ * 
+ * Se obligará a introducir los datos estrictamente necesarios para el cálculo de la nómina
+ * Lo son el IRPF y el Salario Base
+ * 
+ * Requisitos:
+ * - Se deberán introducir los datos en forma de números naturales positivos mayores que 0
+ * 
+ * Entradas:
+ * - Datos del salario
+ * - Número negativo cuando se desee parar la entrada de datos
+ * 
+ * Salidas:
+ * - Devengos
+ * - Deducciones
+ * - BCCC
+ * - BCCP
+ * - Salario bruto
+ * - Salario líquido
+*/
+
+/* Pseudocódigo Generalizado:
+ *	Inicio
+ * 		Declarar variables
+ * 
+ * 		//Bloque de percepciones salariales
+ * 		Leer y validar Salario Base
+ * 		Leer y validar si tiene pluses
+ * 		Mientras quiera introducir pluses
+ * 			Leer y validar entrada de pluses
+ * 		Fin (Mientras quiera introducir pluses)
+ * 		//Fin Bloque de percepciones salariales
+ * 
+ * 		//Bloque de percepciones extrasalariales
+ * 		Leer y validar si tiene percepciones extrasalariales
+ * 		Mientras quiera introducir extrasalarios
+ * 			Leer y validar entrada de extrasalarios
+ * 		Fin (Mientras quiera introducir extrasalarios)
+ * 		//Fin Bloque de percepciones extrasalariales
+ * 
+ * 
+ * 		Leer y validar porcentaje de IRPF
+ * 		Leer y validar si es Temporal o Indefinido
+ * 
+ * 		Leer y validar entrada de datos de deducciones
+ * 		Calcular salario bruto
+ * 		Calcular BCCC
+ * 		Calcular BCCP
+ * 		Calcular salario líquido
+ * 		Mostrar datos
+*/
+
+import java.util.Scanner;
+import java.io.*;
+
+public class fol_salario
+{
+	
+	public static void main (String[] args) 
+	{
+		// Inicializamos las variables
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
