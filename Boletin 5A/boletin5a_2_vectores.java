e7f77bc3930d7896e0e8a5d5e404ab875e3186e9
PenWallet <carterasindinero@gmail.com>
2017-11-20 23:15:06 +0100

    Notengoganasdeescribir

---
+++ Boletin 5A/boletin5a_2_vectores.java
@@ -0,0 +1,132 @@
+/* Nombre del programa: boletin5a_2_vectores
+ * 
+ * Explicación:
+ * Se pedirán dos vectores al usuario, con esos dos vectores, se realizarán 
+ * operaciones, que se detallan en el Análisis
+ * 
+ * Análisis:
+ * - Módulo: Se consigue haciendo la raíz cuadrada de la suma de los 
+ * cuadrados de las dos coordenadas de cada vector
+ *
+ * - Vector suma: Se suman las 'x' y las 'y' de cada vector, y se suman.
+ * 
+ * - Producto escalar: Para este, adicionalmente, se requerirá pedir el
+ * ángulo que forman los dos vectores. Mediante 
+ * 
+ * - Producto vectorial: Este es más difícil y se dejará por ahora en construcción
+ * 
+ * Entradas:
+ * - Coordenadas de vectores, que por simplicidad, solo podrán ser números enteros
+ * - Dentro de Producto Escalar, ángulo que forman los vectores, otro número entero
+ * 
+ * Salida:
+ * - Módulo de los vectores
+ * - Vector suma
+ * - Producto escalar de los vectores
+*/
+
+ // **************************************************
+
+/* Pseudocódigo Generalizado:
+ * 	Inicio
+ * 		Leer y validar coordenadas de vectores
+ * 		Mostrar menú y leer y validar opción
+ * 		Mientras (opcion != 0)
+ * 			Según (opcion)
+ * 				caso 1: Llamar a ModuloVectores y mostrar resultados
+ * 				caso 2: Llamar a VectorSuma y mostrar resultados
+ * 				caso 3: ProductoEscalar y mostrar resultados
+ * 				caso 4: Llamar a ProductoVectorial y mostrar resultados //En construcción
+ * 			Fin Según
+ * 		Fin Mientras
+ * 	Fin
+*/
+
+ // **************************************************
+
+/* Pseudocódigos Específicos:
+ * 
+ * //ModuloVectores
+ * 	Inicio
+ * 		Realizar operaciones
+ * 		Devolver resultado a PP
+ * 	Fin
+ * 
+ * //VectorSuma
+ * 	Inicio
+ * 		Realizar operaciones
+ * 		Devolver resultado a PP
+ * 	Fin
+ * 
+ * //ProductoEscalar
+ * 	Inicio
+ * 		Leer y validar ángulo
+ * 		Realizar operaciones
+ * 		Devolver resultado a PP
+ * 	Fin
+ * 
+*/
+
+ // **************************************************
+
+/* Estudio interfaz:
+ * 
+ * Nombre subprograma: ModuloVectores
+ * Proceso que realiza: Calcula el módulo de dos vectores
+ * Entradas: Dos vectores
+ * Precondiciones: Las coordenadas deben ser número enteros
+ * Salidas: Módulo de vectores
+ * Postcondiciones: Ninguna
+ */
+ 
+ // **************************************************
+ 
+/* Estudio interfaz:
+ * 
+ * Nombre subprograma: VectorSuma
+ * Proceso que realiza: Suma los dos vectores
+ * Entradas: Dos vectores
+ * Precondiciones: Las coordenadas deben ser número enteros
+ * Salidas: Suma de vectores
+ * Postcondiciones: Ninguna
+ */
+ 
+ // **************************************************
+
+/* Estudio interfaz:
+ * 
+ * Nombre subprograma: ProductoEscalar
+ * Proceso que realiza: Realiza el producto escalar 
+ * Entradas: Dos vectores y un ángulo
+ * Precondiciones: Las coordenadas deben ser número enteros
+ * Salidas: Producto escalar
+ * Postcondiciones: Ninguna
+ */
+ 
+ // **************************************************
+ 
+
+
+
+
+import java.util.Scanner;
+import java.io.*;
+
+public class boletin5a_2_vectores
+{
+	private static 
+	
+	public static void main (String[] args) 
+	{
+		//Declarar variables
+		Scanner teclado = new Scanner (System.in);	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
---
+++ Boletin 5A/boletin5a_x_nombre.java
@@ -0,0 +1,35 @@
+/* Nombre del programa:
+ * 
+ * Análisis:
+ * 
+ * 
+ * 
+ * 
+*/
+
+/* Pseudocódigo Generalizado:
+ * 
+ * 
+ * 
+ * 
+*/
+
+import java.util.Scanner;
+import java.io.*;
+
+public class boletin5a_x_nombre
+{
+	
+	public static void main (String[] args) 
+	{
+		//Declarar variables
+		Scanner teclado = new Scanner (System.in);	
+		
+		
+		
+		
+		
+		
+		
+		
+		
