594a0234d6690f506c55e16c44abebd4bb228127
PenWallet <carterasindinero@gmail.com>
2018-01-11 14:23:15 +0100

    nose

---
+++ Boletin 7/rectangulo.java
@@ -0,0 +1,17 @@
+/* Estudio interfaz (clase):
+ * 
+ * - Definición: 
+ * - Por cada propiedad, indicar su tipo y si es consultable/modificable:
+ * - Indicar las propiedades derivadas (fórmula si necesario):
+ * - Indicar si cada propiedad es compartida por los objetos:
+ * - Definir operaciones o funcionalidades del objeto:
+ * 
+*/
+
+public class rectangulo {
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
