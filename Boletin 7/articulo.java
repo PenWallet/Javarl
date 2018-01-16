000973a17836e18ef292a99543085008d2e205d6
PenWallet <carterasindinero@gmail.com>
2018-01-16 10:21:15 +0100

    nojeque

---
+++ Boletin 7/articulo.java
@@ -0,0 +1,29 @@
+/* Estudio interfaz (clase):
+ * 
+ * - Definición:
+ * 		Objeto tipo artículo, que tiene las propiedades comunes a todos los artículos
+ * 		Tendrá nombre, precio sin IVA, fecha de caducidad, IVA y precio de costo (
+ * 
+ * - Por cada propiedad, indicar su tipo y si es consultable/modificable:
+ * 		Nombre: String, consultable y modificable
+ * 		Precio sin IVA: Double, consultable, modificable
+ * 		IVA: Entero, consultable, y modificable
+ * 		Fecha de caducidad: Fecha, consultable y modificable
+ * 		Precio de costo: Double, consultable y modificable
+ * 
+ * - Indicar las propiedades derivadas (fórmula si necesario):
+ * 		Precio: Double, consultable (Precio sin IVA * IVA)
+ * 		
+ * - Indicar si cada propiedad es compartida por los objetos:
+ * 		No comparte ninguna propiedad con los objetos
+ * 
+ * - Definir operaciones o funcionalidades del objeto:
+ * 			
+*/
+
+public class articulo {
+	
+	
+	
+}
+
---
+++ Boletin 7/hora.java
@@ -0,0 +1,28 @@
+/* Estudio interfaz (clase):
+ * 
+ * - Definición:
+ * 		Objeto tipo hora, que se comportará como un reloj, que da la hora, minutos y segundos
+ * 
+ * - Por cada propiedad, indicar su tipo y si es consultable/modificable:
+ * 		Hora: Entero, consultable y modificable
+ * 		Minutos: Entero, consultable, modificable
+ * 		Segundos: Entero, consultable y modificable
+ * 
+ * - Indicar las propiedades derivadas (fórmula si necesario):
+ * - Indicar si cada propiedad es compartida por los objetos:
+ * - Definir operaciones o funcionalidades del objeto:
+ * 		Double getHora()
+ * 		Double getMinutos()
+ * 		Double getSegundos()
+ * 		String getHoraEntera()
+ * 		Void setHora(int hora)
+ * 		Void setMinutos(int minutos)
+ * 		Void setSegundos(int segundos) 		
+*/
+
+public class hora {
+	
+	
+	
+}
+
