bf52cbc3bfc9ae2fc10d9dc8159ff49824ef58a0
PenWallet <carterasindinero@gmail.com>
2018-01-26 00:19:36 +0100

    Cerrojo creado

---
+++ Boletin 7B/Cerrojo/Cerrojo.java
@@ -0,0 +1,210 @@
+/* Estudio interfaz (clase):
+ * 
+ * - Definición:
+ * 		Con esta clase podremos crear objetos de tipo "Cerrojo", que tendrá una
+ * 		combinación, tal y como un cerrojo de verdad. Este se podrá usar como medida
+ * 		de seguridad.
+ * 
+ * 		Tendrá una combinación, que será una secuencia de tres números, y oculta,
+ * 		no se podrán copiar los cerrojos.
+ * 		La combinación solo se podrá cambiar si está desbloqueada
+ *  
+ * 		Como función añadida, si se falla al intentar desbloquearla 3 veces, el cerrojo
+ * 		quedará inservible y no se podrá volver a abrir		
+ * 
+ * - Por cada propiedad, indicar su tipo y si es consultable/modificable:
+ * 		- Numero 1: Entero, modificable
+ * 		- Numero 2: Entero, modificable
+ * 		- Número 3: Entero, modificable
+ * 		- IsBloqueada: Boolean, consultable
+ * 		- Intentos: Byte, consultable (indica cuántas veces se ha intentado abrir la cerradura)
+ * 
+ * - Indicar las propiedades derivadas (fórmula si necesario):
+ * - Indicar si cada propiedad es compartida por los objetos:
+ * - Definir operaciones o funcionalidades del objeto:
+ * 		Void setN1(entero n1)
+ * 		Void setN2(entero n2)
+ * 		Void setN3(entero n3)
+ * 
+ * 		Boolean getBloqueada()
+ * 		Byte getIntentos()
+ * 
+ * 		Void cerrarCerrojo();
+ * 		Boolean abrirCerrojo(entero n1, entero n2, entero n3)
+ * 
+ * - Restricciones:
+ * 		- Cada número de la combinación estará entre 0 y 64 (ambos incluidos)
+ * 		- Los intentos solo podrá llegar hasta 3
+*/
+
+import java.io.*;
+
+public class Cerrojo implements Cloneable
+{
+	//Atributos
+	private int n1;
+	private int n2;
+	private int n3;
+	private boolean isBloqueada;
+	private byte intentos;	
+	
+	//Constructores (no habrá de copia "por seguridad")
+	public Cerrojo()
+	{
+		n1 = 0;
+		n2 = 0;
+		n3 = 0;
+		isBloqueada = false;
+		intentos = 0;
+	}
+	
+	public Cerrojo(int n1, int n2, int n3)
+	{
+		this.n1 = n1;
+		this.n2 = n2;
+		this.n3 = n3;
+		isBloqueada = true;
+		intentos = 0;
+	}
+	
+	//Funcionalidades
+	
+	public void setN1(int n1) throws CerrojoException
+	{
+		if(isBloqueada)
+			throw new CerrojoException("¡El cerrojo está cerrado! No puedes cambiar su combinación");
+		else
+			this.n1 = n1;
+		
+		return;
+	}
+	
+	public void setN2(int n2) throws CerrojoException
+	{
+		if(isBloqueada)
+			throw new CerrojoException("¡El cerrojo está cerrado! No puedes cambiar su combinación");
+		else
+			this.n2 = n2;
+		
+		return;
+	}
+	
+	public void setN3(int n3) throws CerrojoException
+	{
+		if(isBloqueada)
+			throw new CerrojoException("¡El cerrojo está cerrado! No puedes cambiar su combinación");
+		else
+			this.n3 = n3;
+		
+		return;
+	}
+	
+	public boolean getBloqueada() { return(isBloqueada); }
+	public byte getIntentos() { return(intentos); }
+	
+	public void cerrarCerrojo()
+	{
+		this.isBloqueada = true;
+		return;
+	}
+	
+/* Estudio de la Interfaz (funcionalidad):
+ * 
+ * Necesidades: Combinación del cerrojo y combinación del intento
+ * Devoluciones: Boolean
+ * Nec/Dev: Ninguna
+ * Restricciones: Cada número de la combinación deberá estar entre 0 y 64, inclusiveCada número de la combinación deberá estar entre 0 y 64, inclusive
+*/ 
+
+/* Interfaz:
+ * 
+ * Comentario: Función con la que se podrá desbloquear el cerrojo
+ * Cabecera: boolean abrirCerrojo(int n1, int n2, int n3)
+ * Precondiciones: Cada número de la combinación deberá estar entre 0 y 64, inclusive
+ * Entradas: Tres enteros, que es la combinación de intento
+ * Salidas: Un boolean, para indicar si la cerradura se ha abierto o no
+ * Entradas/Salidas: Ninguna
+ * Postcondiciones: El boolean indicando si se ha abierto o no habrá sido devuelto asociado al nombre
+ * 
+*/
+	
+	public boolean abrirCerrojo(int n1, int n2, int n3) throws CerrojoException
+	{
+		boolean isAbierta;
+		
+		if (this.intentos > 2)
+			throw new CerrojoException("¡Esta cerradura está bloqueada! Ha quedado inservible");
+		else if (this.isBloqueada == false)
+			throw new CerrojoException("¡La cerradura ya está abierta!");
+		else if (this.n1 == n1 && this.n2 == n2 && this.n3 == n3)
+		{
+			isAbierta = true;
+			this.isBloqueada = false;
+			this.intentos = 0;
+		}
+		else
+		{
+			isAbierta = false;
+			this.intentos++;
+		}
+		
+		return(isAbierta);
+	}
+	
+	@Override
+	public String toString()
+	{
+		String s = ("N1: "+n1+", N2: "+n2+", N3: "+n3+", Bloqueada: "+isBloqueada+", Intentos: "+intentos+", Hash: "+this.hashCode());
+		return(s);
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		boolean ret = false;
+		
+		if (this == obj)
+			ret = true;
+		else if (obj != null && obj instanceof Cerrojo)
+		{
+			Cerrojo other = (Cerrojo)obj;
+			
+			if (this.n1 == other.n1 &&
+				this.n2 == other.n2 &&
+				this.n3 == other.n3 &&
+				this.isBloqueada == other.isBloqueada &&
+				this.intentos == other.intentos)
+			{
+				ret = true;
+			}
+		}
+		
+		return (ret);
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		int hash;
+		hash = n1 * 17 + n2 * 3 - n3 * 5;
+		return(hash);
+	}
+	
+	//Solo está aquí para poder probar el equals
+	@Override
+	public Cerrojo clone()
+	{
+		Cerrojo copia = null;
+		
+		try
+		{
+			copia = (Cerrojo)super.clone();
+		}catch(CloneNotSupportedException error){System.out.println("¡Clone no creado!");};
+		
+		return (copia);
+	}
+	
+}
+		
+		
+		
---
+++ Boletin 7B/Cerrojo/TestCerrojo.java
@@ -0,0 +1,115 @@
+import java.io.*;
+
+public class TestCerrojo
+{
+	
+	public static void main (String[] args) 
+	{
+		//Crear un cerrojo vacío
+		Cerrojo c1 = new Cerrojo();
+		System.out.println(c1.toString());
+		
+		//******************************************************************************
+		System.out.println("\n\n");
+		//******************************************************************************
+		
+		//Crear un cerrojo nuevo
+		Cerrojo c2 = null;
+		c2 = new Cerrojo(13, 45, 11);
+		System.out.println(c2.toString());
+		
+		//Intentar cambiar combinación con el cerrojo cerrado
+		try
+		{
+			c2.setN1(43);
+			c2.setN2(0);
+			c2.setN3(4);
+		}catch (CerrojoException error){};
+		System.out.println(c2.toString());
+		
+		//******************************************************************************
+		System.out.println("\n\n");
+		//******************************************************************************
+		
+		//Intentar abrir el cerrojo con combinación incorrecta
+		try
+		{
+			System.out.println(c2.abrirCerrojo(13, 45, 12));
+			System.out.println(c2.abrirCerrojo(0, 45, 11));
+		}catch (CerrojoException error){};
+		
+		//******************************************************************************
+		System.out.println("\n\n");
+		//******************************************************************************
+		
+		//Intentar abrir el cerrojo con combinación correcta
+		try
+		{
+			System.out.println(c2.abrirCerrojo(13, 45, 11));
+			
+		}catch (CerrojoException error){};
+		
+		//******************************************************************************
+		System.out.println("\n\n");
+		//******************************************************************************
+		
+		//Intentar cambiar combinación con el cerrojo abierto
+		try
+		{
+			c2.setN1(43);
+			c2.setN2(0);
+			c2.setN3(4);
+		}catch (CerrojoException error){};
+		System.out.println(c2.toString());
+		
+		//******************************************************************************
+		System.out.println("\n\n");
+		//******************************************************************************
+		
+		//Bloquear la cerradura
+		c2.cerrarCerrojo();
+		System.out.println(c2.toString());
+		
+		try
+		{
+			System.out.println(c2.abrirCerrojo(19, 0, 4));
+			System.out.println(c2.toString());
+			System.out.println(c2.abrirCerrojo(43, 64, 4));
+			System.out.println(c2.toString());
+			System.out.println(c2.abrirCerrojo(43, 0, 10));
+			System.out.println(c2.toString());
+		}catch (CerrojoException error){};
+		
+		
+		//******************************************************************************
+		System.out.println("\n\n");
+		//******************************************************************************
+		
+		//Intentar abrir la cerradura con tres intentos
+		try
+		{
+			System.out.println(c2.abrirCerrojo(43, 0, 4));
+			System.out.println(c2.toString());
+		}catch (CerrojoException error){};
+		
+		//******************************************************************************
+		System.out.println("\n\n");
+		//******************************************************************************
+		
+		//Es c1 equal a c2
+		System.out.println(c1.equals(c2));
+		
+		//******************************************************************************
+		System.out.println("\n\n");
+		//******************************************************************************
+		
+		//Crear una copia de c2
+		Cerrojo c3 = null;
+		c3 = c2.clone();
+		
+		//Probar el equals
+		System.out.println(c3.equals(c2));
+	}
+}
+		
+		
