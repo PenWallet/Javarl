02877ceade7c6291e03704fcd0a38f4aa5913639
PenWallet <carterasindinero@gmail.com>
2018-02-22 13:07:51 +0100

    ik,l

---
+++ Boletin 8.2/Buscaminas/Buscaminas.java
@@ -0,0 +1,53 @@
+/* Nombre del programa: Buscaminas
+ * 
+ * Análisis:
+ *  Comentario:
+ *  Juego del buscaminas, donde se usará un array cargado de objetos de la clase Casilla, que tendrá
+ *  la información necesaria de cada casilla del grid
+ *  
+ *  Entradas:
+ *  - Número entero de fila
+ *  - Número entero de columna
+ *  
+ *  Salidas:
+ *  - Grid del juego, con información actualizada
+ *  
+ *  Restricciones:
+ *  - El número de la fila y de la columna debe ser mayor que 0 y menor que sus límites
+ * 
+*/
+
+/* Pseudocódigo Generalizado:
+ * 	Inicio
+ * 		Cargar array aleatoriamente
+ * 		Repetir
+ * 			MostrarGrid *
+ * 			Leer y validar opcion
+ * 			Según
+ * 				Caso 1: Flag a box
+ * 				Caso 2: Seleccionar box
+ * 			Fin Según
+ * 
+ * 
+*/
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Buscaminas
+{
+	
+	public static void main (String[] args) 
+	{
+		//Declarar variables
+		Scanner teclado = new Scanner (System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
---
+++ Boletin 8.2/Buscaminas/Casilla.java
@@ -0,0 +1,31 @@
+/* Estudio interfaz (clase):
+ * 
+ * - Definición: 
+ * 	Clase 'Casilla', que se usará para implementar el juego 'Buscaminas'. Cada objeto de
+ * 	esta clase representará una casilla del tablero del Buscaminas
+ * 
+ * - Por cada propiedad, indicar su tipo y si es consultable/modificable:
+ * 	- isBomb: Boolean, consultable
+ * 	- isFlagged: Boolean, consultable, modificable
+ * 
+ * - Indicar las propiedades derivadas (fórmula si necesario):
+ * - Indicar si cada propiedad es compartida por los objetos:
+ * - Definir operaciones o funcionalidades del objeto:
+ * 
+*/
+
+import java.io.*;
+
+public class Casilla
+{
+	private boolean isBomb;
+	
+	
+	
+	
+	
+	
+		
+		
+		
+		
