b7cf8cadd6675c40ca9afbc087fb0b6ef56616c0
PenWallet <carterasindinero@gmail.com>
2018-04-04 13:51:08 +0200

    Muxas cosa

---
+++ Boletin 8.2/Sea of Zifs/src/pruebaPantalla.java
@@ -0,0 +1,23 @@
+public class pruebaPantalla {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print("·········|");
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i + 1);
+        }
+
+        //Resultado: 180 x 44
+
+        //Nombre de las islas:
+        /*
+        Calamah Island
+        Happiness Island //Aquí se empieza
+        Null Island
+        Cucumber Island
+        Bolsillodedoraimon Island
+        Tritetua Island
+        Staling Island
+         */
+    }
+}
