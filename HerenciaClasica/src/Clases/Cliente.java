ef3b9ba863bc208b6918b5769ae37ade355414e1
Unknown <carterasindinero@gmail.com>
2018-05-16 23:09:15 +0200

    asd

--- HerenciaClasica/src/Clases/Cliente.java
+++ HerenciaClasica/src/Clases/Cliente.java
@@ -15,19 +15,91 @@ package Clases;
 
             int getID()
 
+            void reducirDinero(double cantidad)
+
 
 */
+
+/**
+ * Clase Cliente, para usar en un programa de gestión de alquiler de películas
+ */
 public class Cliente extends Persona {
 
+    /**
+     * El dinero que el cliente dispone
+     */
     private double dinero;
+
+    /**
+     * El ID del cliente
+     */
     private int ID;
+
+    /**
+     * Contador de IDs para poder asignarle una ID única a cada uno
+     */
     private static int IDCont = 0;
 
+    //CONSTRCUTORES
 
+    /**
+     * Constructor por defecto de Cliente
+     */
     public Cliente()
     {
         super();
         this.dinero = 0;
         this.ID = IDCont++;
     }
+
+    /**
+     * Constructor con parámetros de Cliente
+     */
+    public Cliente(String nombre, int edad, String DNI, char genero, double dinero)
+    {
+        super(nombre, edad, DNI, genero);
+        this.dinero = dinero;
+        this.ID = IDCont++;
+    }
+
+    /**
+     * Constructor de copia de Cliente
+     */
+    public Cliente(Cliente c)
+    {
+        this.nombre = c.nombre;
+        this.edad = c.edad;
+        this.DNI = c.DNI;
+        this.genero = c.genero;
+        this.dinero = c.dinero;
+        this.ID = c.ID;
+    }
+
+    //GETTERS Y SETTERS
+
+    /**
+     * Devuelve el dinero
+     *
+     * @return El dinero
+     */
+    public double getDinero() { return(this.dinero); }
+
+    /**
+     * Reemplaza el dinero existente y le coloca un valor nuevo
+     *
+     * @param dinero La nueva cantidad de dinero disponible
+     */
+    public void setDinero(double dinero) { this.dinero = dinero; }
+
+    /**
+     * Devuelve la ID
+     *
+     * @return La ID
+     */
+    public int getID() { return(this.ID); }
+
+    //FUNCIONES AÑADIDAS
+
+
+
 }
